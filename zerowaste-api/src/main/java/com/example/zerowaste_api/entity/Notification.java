e1feab226dc08b17afbb799bb1a9d00ef3ceb766
codewhat7 <feiweng579@gmail.com>
2025-09-26 22:12:43 +0800

    Notification entity

---
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/entity/Notification.java
@@ -0,0 +1,29 @@
+package com.example.zerowaste_api.entity;
+
+import com.example.zerowaste_api.common.BaseDomain;
+import com.example.zerowaste_api.enums.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigInteger;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="notification")
+
+public class Notification extends BaseDomain {
+
+    private BigInteger usersId;
+
+    @Enumerated (EnumType.STRING)
+    private Status status;
+
+    private String message;
+
+    private Boolean markAsRead;
+
+    pric
+}
