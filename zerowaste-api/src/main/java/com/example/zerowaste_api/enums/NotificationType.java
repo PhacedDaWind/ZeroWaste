34a784e68f313fcdb072b878a7f292e7aac51481
codewhat7 <feiweng579@gmail.com>
2025-09-26 22:14:12 +0800

    Notification type enum ori

---
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/enums/NotificationType.java
@@ -0,0 +1,7 @@
+package com.example.zerowaste_api.enums;
+
+import com.example.zerowaste_api.common.BaseEnumInterface;
+
+public enum NotificationType extends BaseEnumInterface {
+    FOOD_INVENTORY_ADDED("Food i")
+}
