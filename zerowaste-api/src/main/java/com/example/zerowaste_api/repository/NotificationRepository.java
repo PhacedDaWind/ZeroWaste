0ea00e58791cb1bf975004c06f1769c378a1a0c6
codewhat7 <feiweng579@gmail.com>
2025-09-27 15:53:50 +0800

    notification fetch all notification and unread notification

--- zerowaste-api/src/main/java/com/example/zerowaste_api/repository/NotificationRepository.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/repository/NotificationRepository.java
@@ -2,6 +2,7 @@ package com.example.zerowaste_api.repository;
 
 import com.example.zerowaste_api.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,9 +13,16 @@ import java.util.Optional;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
-    //fetch all notification for user sorted by most recent
-    @Query("SELECT n FROM Notification n WHERE n.usersId=:userId ORDER BY n.createdAT DESC")
+    //fetch all notification for a user sorted by most recent
+    @Query("SELECT u FROM Notification u WHERE u.usersId=:userId ORDER BY u.createdAt DESC")
     List<Notification> findByUserId(@Param("userId") BigInteger userId);
 
-    @Query("")
+    //fetch all notification for a user that is unread
+    @Query("SELECT u FROM Notification u WHERE u.usersId=:usersId AND u.markAsRead=false ORDER BY u.createdAt DESC")
+    List<Notification> findByUnread(@Param("usersId") BigInteger usersId);
+
+
+    @Modifying
+    @Query("UPDATE Notification n SET ")
 }
+
