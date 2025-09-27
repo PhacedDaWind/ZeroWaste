6941e0a9d75785a51454866aefe9c848d32e3efb
codewhat7 <feiweng579@gmail.com>
2025-09-27 15:28:45 +0800

    fetch a user all notiifications sorted by most recent

--- zerowaste-api/src/main/java/com/example/zerowaste_api/repository/NotificationRepository.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/repository/NotificationRepository.java
@@ -3,12 +3,18 @@ package com.example.zerowaste_api.repository;
 import com.example.zerowaste_api.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigInteger;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
+    //fetch all notification for user sorted by most recent
+    @Query("SELECT n FROM Notification n WHERE n.usersId=:userId ORDER BY n.createdAT DESC")
+    List<Notification> findByUserId(@Param("userId") BigInteger userId);
 
-    Optional<Notification> findByIdAndUsersId(Long id, Long userId);
+    @Query("")
 }
