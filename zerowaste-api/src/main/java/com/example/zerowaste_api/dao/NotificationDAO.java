da02c21eec7a436c30b0ea7b96ad814449e739f1
codewhat7 <feiweng579@gmail.com>
2025-09-28 00:14:56 +0800

    notification dao update

--- zerowaste-api/src/main/java/com/example/zerowaste_api/dao/NotificationDAO.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/dao/NotificationDAO.java
@@ -1,7 +1,24 @@
 package com.example.zerowaste_api.dao;
 
+import com.example.zerowaste_api.entity.Notification;
+import com.example.zerowaste_api.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class NotificationDAO {
+    private final NotificationRepository notificationRepository;
+
+    public NotificationDAO(NotificationRepository notificationRepository) {
+        this.notificationRepository=notificationRepository;
+    }
+
+
+    public Notification save(Notification notification) {
+        if (Objects.isNull(notification)) {
+            return null;
+        }
+        return notificationRepository.save(notification);
+    }
 }
