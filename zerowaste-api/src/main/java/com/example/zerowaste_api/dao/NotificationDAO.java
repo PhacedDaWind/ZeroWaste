b3c0fcfb2b86e84c3e2cb04fc7888ffbe6818e63
codewhat7 <feiweng579@gmail.com>
2025-09-28 16:13:37 +0800

    notification DAO return repository

--- zerowaste-api/src/main/java/com/example/zerowaste_api/dao/NotificationDAO.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/dao/NotificationDAO.java
@@ -4,6 +4,8 @@ import com.example.zerowaste_api.entity.Notification;
 import com.example.zerowaste_api.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -20,5 +22,25 @@ public class NotificationDAO {
             return null;
         }
         return notificationRepository.save(notification);
+    }
+
+    public List<Notification> findbyUserId (BigInteger userId){
+        return notificationRepository.findByUserId(userId);
+    }
+
+    public List<Notification> findbyUnread (BigInteger userId){
+        return notificationRepository.findByUnread(userId);
+    }
+
+    public void markAsRead (BigInteger notificationId, BigInteger userId) {
+        notificationRepository.changeToRead(notificationId, userId);
+    }
+
+    public void deleteByNotificationId(BigInteger notificationId) {
+        notificationRepository.deleteByNotificationId(notificationId);
+    }
+
+    public void deleteByUserId(BigInteger userId) {
+        notificationRepository.deleteWholeNotifications(userId);
     }
 }
