19ac3d7826d4ce07e0dec20dd7f6ce464659cf00
codewhat7 <feiweng579@gmail.com>
2025-10-02 15:19:44 +0800

    notification converter done

--- zerowaste-api/src/main/java/com/example/zerowaste_api/converter/NotificationConverter.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/converter/NotificationConverter.java
@@ -1,26 +1,27 @@
 package com.example.zerowaste_api.converter;
 
 
-import com.example.zerowaste_api.dto.NotificationReqDTO;
 import com.example.zerowaste_api.dto.NotificationResDTO;
 import com.example.zerowaste_api.entity.Notification;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Service
 public class NotificationConverter {
 
-    public NotificationResDTO toNotification(Notification notification) {
+    public NotificationResDTO toNotificationResDTO(Notification notification) {
         if(Objects.isNull(notification)){
             return null;
         }
         NotificationResDTO response= new NotificationResDTO();
+        response.setId(notification.getId());
+        response.setCreatedAt(notification.getCreatedAt());
         response.setUsersId(notification.getUsersId());
-        response.setStatus(notification.getStatus());
         response.setMessage(notification.getMessage());
         response.setMarkAsRead(notification.getMarkAsRead());
+        response.setNotifType(notification.getNotifType());
         return response;
     }
-
 }
