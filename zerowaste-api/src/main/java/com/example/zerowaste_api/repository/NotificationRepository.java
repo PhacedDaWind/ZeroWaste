1d975f46a87d5cd08b1d3568f8ed5526c9580b00
codewhat7 <feiweng579@gmail.com>
2025-10-02 15:20:44 +0800

    notification repository done

--- zerowaste-api/src/main/java/com/example/zerowaste_api/repository/NotificationRepository.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/repository/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.example.zerowaste_api.repository;
 
 import com.example.zerowaste_api.entity.Notification;
+import com.example.zerowaste_api.enums.NotificationType;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,26 +16,17 @@ import java.util.Optional;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
     //fetch all notification for a user sorted by most recent
-    @Query("SELECT u FROM Notification u WHERE u.usersId=:userId ORDER BY u.createdAt DESC")
-    List<Notification> findByUserId(@Param("userId") BigInteger userId);
+    @Query("SELECT u FROM Notification u WHERE u.usersId=:userId AND (:notifType IS NULL OR u.notifType = :notifType) ORDER BY u.createdAt DESC")
+    List<Notification> findByUserIdAndNotifType(@Param("userId") Long userId, @Param("notifType")NotificationType notifType);
 
     //fetch all notification for a user that is unread
-    @Query("SELECT u FROM Notification u WHERE u.usersId=:usersId AND u.markAsRead=false ORDER BY u.createdAt DESC")
-    List<Notification> findByUnread(@Param("usersId") BigInteger usersId);
-
-    //mark specific unread notification as Read?
-    @Modifying
-    @Query("UPDATE Notification u SET u.markAsRead=True WHERE u.id=:notificationId AND u.usersId=:usersId AND u.markAsRead=false")
-    void changeToRead(@Param("notificationId") BigInteger id, @Param("usersId") BigInteger usersId);
-
-    //Delete specific notification
-    @Modifying
-    @Query("DELETE FROM Notification u WHERE u.id=:notificationId")
-    void deleteByNotificationId(@Param("notificationId") BigInteger id);
+    @Query("SELECT u FROM Notification u WHERE u.usersId=:userId AND u.markAsRead=false ORDER BY u.createdAt DESC")
+    List<Notification> findByUnread(@Param("userId") Long userId);
 
     //Delete whole notification for a specific user
+    @Transactional
     @Modifying
     @Query("DELETE FROM Notification u WHERE u.usersId=:usersId ")
-    void deleteWholeNotifications(@Param("usersId") BigInteger usersId);
+    void deleteWholeNotifications(@Param("usersId") Long usersId);
 }
 
