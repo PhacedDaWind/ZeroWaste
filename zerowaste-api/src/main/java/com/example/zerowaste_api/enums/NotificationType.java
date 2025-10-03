02a0051cc2198cd52fa97b12863aa44b9b7d0f14
codewhat7 <feiweng579@gmail.com>
2025-10-03 14:03:38 +0800

    notification enum type maybe got error

--- zerowaste-api/src/main/java/com/example/zerowaste_api/enums/NotificationType.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/enums/NotificationType.java
@@ -2,8 +2,12 @@ package com.example.zerowaste_api.enums;
 
 import com.example.zerowaste_api.common.BaseEnumInterface;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
 
 public enum NotificationType implements BaseEnumInterface {
     FOOD_INVENTORY_ALERT("$food is nearing expiry on $date!! Consider to use it soon or convert to donation?"),
@@ -21,7 +25,6 @@ public enum NotificationType implements BaseEnumInterface {
         return label;
     }
 
-
     @SuppressWarnings("unchecked")
     public <T> T getValue(){
         return (T) label;
@@ -33,19 +36,41 @@ public enum NotificationType implements BaseEnumInterface {
         switch (this){
             case FOOD_INVENTORY_ALERT:
                 if(extra.length>=2) {
-                    result = result.replace("$food", String.valueOf(extra[0]));
-                    result = result.replace("$date", String.valueOf(extra[1]));
+                    List<String> food=(List<String>)extra[0];
+                    String foodString = String.join(", ", food);
+                    result = result.replace("$food", foodString);
+                    LocalDate expiry=(LocalDate)extra[1];
+                    result = result.replace("$date", formDate(expiry));
                 }
                 break;
-            case DONATION_CLAIMED:
-                if(extra.length>=1) {
-                    result=result.replace("$foods", String.valueOf(extra[0]));
+            case DONATION_POSTED:
+                if(extra.length>=2 && extra[0] != null && extra[1] != null) {
+                    @SuppressWarnings("unchecked")
+                         List<String> foods=(List<String>) extra[0];
+                    @SuppressWarnings("unchecked")
+                            List<Long> quantity=(List<Long>) extra[1];
+
+                    if(foods.size()!=quantity.size()) {
+                        throw new IllegalArgumentException("Number of foods and quantity are not equal");
+                    }
+                    String foodList="";
+                    for(int i=0;i<foods.size();i++) {
+                        foodList=foodList.concat(foods.get(i))
+                                .concat("(")
+                                .concat(String.valueOf(quantity.get(i)))
+                                .concat(")");
+                        if(i<foods.size()-1){
+                            foodList=foodList.concat(",");
+                        }
+                    }
+                    result = result.replace("$foods", foodList);
                 }
                 break;
             case MEAL_REMINDER:
                 if(extra.length>=2) {
                     result=result.replace("$meal", String.valueOf(extra[0]));
-                    result=result.replace("$date", String.valueOf(extra[1]));
+                    LocalDate expiry=(LocalDate)extra[1];
+                    result=result.replace("$date", formDate(expiry));
                 }
                 break;
             default:
@@ -55,7 +80,8 @@ public enum NotificationType implements BaseEnumInterface {
     }
 
     //format date time
-    private String formDate(LocalDateTime time){
-            return time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    public String formDate(LocalDate date){
+        DateTimeFormatter sf=DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return sf.format(date);
     }
 }
