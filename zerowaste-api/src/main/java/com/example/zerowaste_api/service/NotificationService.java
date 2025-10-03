1ffe4379eaa3325e5fad0d5b002cc850054a8363
codewhat7 <feiweng579@gmail.com>
2025-10-03 13:50:36 +0800

    notification service done but have issue maybe

--- zerowaste-api/src/main/java/com/example/zerowaste_api/service/NotificationService.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/service/NotificationService.java
@@ -11,6 +11,9 @@ import com.example.zerowaste_api.enums.NotificationType;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -34,46 +37,81 @@ public class NotificationService {
     }
 
     public List<NotificationResDTO> unreadNotificationList(Long id) {
-        List<Notification> unreadList= notificationDAO.findByUnread(id);
-        return unreadList.stream().map(notif->{
+        List<Notification> unreadList = notificationDAO.findByUnread(id);
+        return unreadList.stream().map(notif -> {
             return notificationConverter.toNotificationResDTO(notif);
         }).collect(Collectors.toList());
     }
 
     public NotificationResDTO markAsRead(Long id) {
-        Notification notification= notificationDAO.findById(id);
-        if(notification.getMarkAsRead()==false) {
+        Notification notification = notificationDAO.findById(id);
+        if (notification.getMarkAsRead() == false) {
             notification.setMarkAsRead(true);
             notificationDAO.save(notification);
         }
         return notificationConverter.toNotificationResDTO(notification);
     }
 
-    public void delete(Long id){
-        if ( Objects.isNull(id)) {
+    public void delete(Long id) {
+        if (Objects.isNull(id)) {
             throw new ServiceAppException(HttpStatus.BAD_REQUEST, FoodItemErrorConstant.FOOD_ITEM_NOT_FOUND);
         }
         notificationDAO.delete(id);
     }
 
-    public void deleteAll(Long userId){
+    public void deleteAll(Long userId) {
         if (Objects.isNull(userId)) {
             throw new ServiceAppException(HttpStatus.BAD_REQUEST, "User ID cannot be null");
         }
         notificationDAO.deleteByUserId(userId);
     }
 
-//    public NotificationResDTO createFoodItemNotification(String itemName, Long quantity, Long userId) {
-//        //new notifcation entity
-//        Notification notification = new Notification();
-//
-//        //converter to update notif (need to switch case for diff notif type)
-//        notificationConverter.toNotifcation();
-//
-//        //save entity
-//        notificationDAO.save(notification);
-//
-//        //return response
-//        return notificationConverter.toNotificationResDTO(notification)
-//    }
+    public NotificationResDTO create(NotificationType notificationType,
+                                     Long userId,
+                                     List<String> itemName,
+                                     List<Long> quantity,
+                                     LocalDate expiryDate,
+                                     String meal) {
+        //new notifcation entity)
+        Notification notification = new Notification();
+        notification.setUsersId(userId);
+        notification.setNotifType(notificationType);
+        String message;
+        //converter to update notif (need to switch case for diff notif type)
+        switch (notificationType) {
+            case FOOD_INVENTORY_ALERT:
+                if (expiryDate == null) {
+                    throw new IllegalArgumentException("Expiry date required for FOOD_INVENTORY_ALERT");
+                }
+                Long diff = ChronoUnit.DAYS.between(LocalDate.now(), expiryDate);
+                if (diff >= 0 && diff <= 3) {
+                    message = notificationType.format(itemName, expiryDate);
+                    notification.setMessage(message);;
+                } else {
+                    return null;
+                }
+                break;
+
+            case DONATION_POSTED:
+                if (itemName == null || quantity == null || itemName.size() != quantity.size()) {
+                    throw new IllegalArgumentException("Item names and quantities must match for DONATION_POSTED");
+                }
+                message = notificationType.format(itemName, quantity);
+                notification.setMessage(message);
+                break;
+            case DONATION_CLAIMED:
+                message = notificationType.getLabel();
+                notification.setMessage(message);
+                break;
+            case MEAL_REMINDER:
+                message = notificationType.format(meal,expiryDate);
+                notification.setMessage(message);;
+                break;
+
+            default:
+                throw new IllegalArgumentException("Invalid Notification Type!!");
+        }
+        notificationDAO.save(notification);
+        return notificationConverter.toNotificationResDTO(notification);
+    }
 }
