d40db0a551fbdd83e6e2d717089b688e44aac4b8
codewhat7 <feiweng579@gmail.com>
2025-10-09 09:37:25 +0800

    Browse Food Item Res DTO

--- zerowaste-api/src/main/java/com/example/zerowaste_api/dto/BrowseFoodItemResDTO.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/dto/BrowseFoodItemResDTO.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 public class BrowseFoodItemResDTO {
     private String name;
 
-    private BigDecimal quantity;
+    private Long quantity;
 
     private LocalDate expiryDate;
 
