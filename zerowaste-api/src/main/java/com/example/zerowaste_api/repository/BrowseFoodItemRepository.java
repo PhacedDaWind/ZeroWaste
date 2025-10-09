6b8e0554b952843e6bc4b588bba58c6192f89d96
codewhat7 <feiweng579@gmail.com>
2025-10-09 13:16:46 +0800

    browse food item repo latest

--- zerowaste-api/src/main/java/com/example/zerowaste_api/repository/BrowseFoodItemRepository.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/repository/BrowseFoodItemRepository.java
@@ -14,20 +14,24 @@ import java.time.LocalDate;
 @Repository
 public interface BrowseFoodItemRepository extends JpaRepository<FoodItem,Long> {
     @Query("SELECT u.id as id, "
+            +"e.id as usersId, "
+            +"u.convertToDonation as convertToDonation, "
+            +"u.category as category, "
+            +"u.expiryDate as expiryDate,"
+            +"u.storageLocation as storageLocation, "
             +"u.name as userName, "
             +"u.quantity as quantity,"
-            +"u.expiryDate as expiryDate,"
             +"u.pickupLocation as pickupLocation,"
             +"u.contactMethod as contactMethod "
             +"FROM FoodItem u "
             +"LEFT JOIN Users e "
             +"WHERE (:usersId IS NULL OR e.id=:usersId ) AND "
             +"(:convertToDonation IS NULL OR u.convertToDonation = :convertToDonation) AND "
-            +"(:category IS NULL OR u.category LIKE :category) AND "
-            +"(:expiryDate IS NULL OR u.expiryDate=:expiryDate) AND"
+            +"(:category IS NULL OR u.category LIKE :category) AND  "
+            +"(:expiryDate IS NULL OR u.expiryDate=:expiryDate) AND "
             +"(:storageLocation IS NULL OR u.storageLocation LIKE :storageLocation)")
     Page<BrowseFoodItemTuple> getBrowse(Pageable pageable,
-                                        @Param("userId")Long userId,
+                                        @Param("usersId")Long usersId,
                                         @Param("convertToDonation")Boolean convertToDonation,
                                         @Param("category")String category,
                                         @Param("expiryDate") LocalDate expiryDate,
