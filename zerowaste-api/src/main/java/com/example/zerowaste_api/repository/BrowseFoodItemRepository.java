8e9cf85e646f74598fe24585ade3d74b1f3ab984
codewhat7 <feiweng579@gmail.com>
2025-10-09 09:37:03 +0800

    Browse Food Item Repository

---
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/repository/BrowseFoodItemRepository.java
@@ -0,0 +1,35 @@
+package com.example.zerowaste_api.repository;
+
+import com.example.zerowaste_api.dto.BrowseFoodItemTuple;
+import com.example.zerowaste_api.entity.FoodItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+
+@Repository
+public interface BrowseFoodItemRepository extends JpaRepository<FoodItem,Long> {
+    @Query("SELECT u.id as id, "
+            +"u.name as userName, "
+            +"u.quantity as quantity,"
+            +"u.expiryDate as expiryDate,"
+            +"u.pickupLocation as pickupLocation,"
+            +"u.contactMethod as contactMethod "
+            +"FROM FoodItem u "
+            +"LEFT JOIN Users e "
+            +"WHERE (:usersId IS NULL OR e.id=:usersId ) AND "
+            +"(:convertToDonation IS NULL OR u.convertToDonation = :convertToDonation) AND "
+            +"(:category IS NULL OR u.category LIKE :category) AND "
+            +"(:expiryDate IS NULL OR u.expiryDate=:expiryDate) AND"
+            +"(:storageLocation IS NULL OR u.storageLocation LIKE :storageLocation)")
+    Page<BrowseFoodItemTuple> getBrowse(Pageable pageable,
+                                        @Param("userId")Long userId,
+                                        @Param("convertToDonation")Boolean convertToDonation,
+                                        @Param("category")String category,
+                                        @Param("expiryDate") LocalDate expiryDate,
+                                        @Param("storageLocation")String storageLocation);
+}
