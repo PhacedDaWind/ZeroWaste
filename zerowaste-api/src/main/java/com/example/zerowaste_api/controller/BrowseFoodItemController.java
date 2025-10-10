69ceb4c7f323cd749a6fd80b507389e986a92a69
codewhat7 <feiweng579@gmail.com>
2025-10-10 17:30:31 +0800

    Browse Food Item Controller Done

--- zerowaste-api/src/main/java/com/example/zerowaste_api/controller/BrowseFoodItemController.java
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/controller/BrowseFoodItemController.java
@@ -3,15 +3,21 @@ package com.example.zerowaste_api.controller;
 import com.example.zerowaste_api.common.BaseController;
 import com.example.zerowaste_api.common.PageWrapperVO;
 import com.example.zerowaste_api.common.ResponseDTO;
+import com.example.zerowaste_api.dto.BrowseFoodItemATResDTO;
 import com.example.zerowaste_api.dto.BrowseFoodItemReqDTO;
 import com.example.zerowaste_api.dto.BrowseFoodItemResDTO;
+import com.example.zerowaste_api.enums.FoodItemActionType;
 import com.example.zerowaste_api.service.BrowseFoodItemService;
+import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RestController
+@Controller
 @RequestMapping("api/browse-food")
 public class    BrowseFoodItemController extends BaseController {
 
@@ -22,7 +28,13 @@ public class    BrowseFoodItemController extends BaseController {
     }
 
     @GetMapping("/list")
-    public ResponseDTO<PageWrapperVO<BrowseFoodItemResDTO>> showList(@RequestBody(required = false) BrowseFoodItemReqDTO browseFoodItemReqDTO) {
+    public ResponseDTO<PageWrapperVO<BrowseFoodItemResDTO>> showList( @Valid @ParameterObject BrowseFoodItemReqDTO browseFoodItemReqDTO) {
         return createResponse(HttpStatus.OK, browseFoodItemService.getBrowseList(browseFoodItemReqDTO));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseDTO<BrowseFoodItemATResDTO> chooseActionType(@PathVariable Long id,
+                                                             @RequestParam FoodItemActionType  foodItemActionType) {
+        return createResponse(HttpStatus.OK, browseFoodItemService.chooseActionType(id, foodItemActionType));
     }
 }
