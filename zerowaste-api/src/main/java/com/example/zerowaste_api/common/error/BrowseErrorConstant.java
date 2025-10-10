17229d5f5809a990e55e4d0880e608e76cce34ba
codewhat7 <feiweng579@gmail.com>
2025-10-10 17:29:28 +0800

    Browse Error Constant updated

---
+++ zerowaste-api/src/main/java/com/example/zerowaste_api/common/error/BrowseErrorConstant.java
@@ -0,0 +1,6 @@
+package com.example.zerowaste_api.common.error;
+
+public class BrowseErrorConstant {
+    public static final String BROWSE_FOOD_ITEM_NOT_FOUND = "browseFoodItem.notFound";
+    public static final String BROWSE_FOOD_ITEM_INVALID_REQUEST = "browseFoodItem.invalid.request";
+}
